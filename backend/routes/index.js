9d0308e5c42dcf30d4ec1de77a90afb39145e9e0
brad <bradsherwood24816@gmail.com>
2021-07-15 12:34:55 -0700

    phase 1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const csrf = require('csurf')
+const csrfProtection = csrf({ cookie: true })
+const apiRouter = require('./api');
 
-router.get('/hello/world', function (req, res) {
+router.use('/api', apiRouter);
+
+router.get('/hello/world', csrfProtection, function (req, res) {
     res.cookie('XSRF-TOKEN', req.csrfToken());
     res.send('Hello World!');
 });
